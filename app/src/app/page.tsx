f22add71efc86379edd790f1d626c57658826f72
areesha-22 <159808338+saima498@users.noreply.github.com>
2024-12-08 10:41:27 +0500

    Update page.tsx

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -2,7 +2,7 @@
 
 export default function Home() {
   return (
-  <div className="bg-orange-500 text-[20px] text-center">hello world</div>
+  <div className="bg-orange-500 text-[20px] text-center">hell world</div>
   
   );
 }
